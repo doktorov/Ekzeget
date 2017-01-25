a95618550d83445c12176e8b5312400f40cab09c
doktorov <doktorov.dmitriy@gmail.com>
2017-01-26 00:22:30 +0700

    Адаптер для спина

---
+++ app/src/main/java/ekzeget/ru/ekzeget/Book.java
@@ -0,0 +1,13 @@
+package ekzeget.ru.ekzeget;
+
+public class Book {
+    public String key;
+    public String name;
+    public String short_name;
+    public int parts;
+
+    public String toString()
+    {
+        return short_name;
+    }
+}
--- app/src/main/java/ekzeget/ru/ekzeget/MainActivity.java
+++ app/src/main/java/ekzeget/ru/ekzeget/MainActivity.java
@@ -4,9 +4,11 @@ import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
@@ -16,34 +18,56 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.adapter.rxjava.HttpException;
 import rx.Observable;
 import rx.functions.Action1;
 
 public class MainActivity extends AppCompatActivity {
-
-    String[] data = {"Кн.", "Мф.", "Мк.", "Лк.", "Ин.", "Деян.", "Иак.", "1 Пет.", "2 Пет.", "1 Ин.", "2 Ин.", "3 Ин.",
-            "Иуд.", "Рим.", "1 Кор.", "2 Кор.", "Гал.", "Еф.", "Флп.", "Кол.", "1 Фес.", "2 Фес.", "1 Тим.",
-            "2 Тим.", "Тит.", "Флм.", "Евр.", "Откр.", "Быт.", "Исх.", "Лев.", "Чис.", "Втор.", "Нав.", "Суд.",
-            "Руф.", "1 Цар.", "2 Цар.", "3 Цар.", "4 Цар.", "1 Пар.", "2 Пар.", "1 Езд.", "Неем.", "2 Езд.",
-            "Тов.", "Иудиф.", "Есф.", "Иов.", "Пс.", "Притч.", "Еккл.", "Песн.", "Прем.", "Сир.", "Ис.",
-            "Иер.", "Плач.", "Посл.", "Вар.", "Иез.", "Дан.", "Ос.", "Иоил.", "Ам.", "Авд.", "Ион.", "Мих.",
-            "Наум.", "Авв.", "Соф.", "Агг.", "Зах.", "Мал.", "1 Мак.", "2 Мак.", "3 Мак.", "3 Езд."};
-
     String[] data2 = {"Гл."};
+
+    GsonBooks gsonBooks = new GsonBooks();
+
+    ArrayList<Book> books = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        InputStream XmlFileInputStream = getResources().openRawResource(R.raw.books);
+        String sxml = readTextFile(XmlFileInputStream);
+        Gson gson = new Gson();
+        gsonBooks = gson.fromJson(sxml, GsonBooks.class);
+
+        for (GsonBooks.Book item : gsonBooks.nz) {
+            Book book = new Book();
+            book.short_name = item.short_name;
+            book.key = item.key;
+            books.add(book);
+        }
+
         // адаптер
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, data);
+        /*ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, data);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        spinner.setAdapter(adapter);
+        final ArrayAdapter<GsonBooks.Book> spinnerArrayAdapter = new ArrayAdapter<GsonBooks.Book>(this, android.R.layout.simple_spinner_item, gsonBooks.getNz()){
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+
+                TextView tv = (TextView) view;
+                tv.setText(gsonBooks.getNz().get(position).short_name);
+                return view;
+            }
+        };
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);*/
+
+        ArrayAdapter<Book> myAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, books);
+        final Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setAdapter(myAdapter);
         // заголовок
         spinner.setPrompt("Title");
         // выделяем элемент
@@ -67,11 +91,6 @@ public class MainActivity extends AppCompatActivity {
         spinner2.setPrompt("Title");
         spinner2.setSelection(0);
 
-        InputStream XmlFileInputStream = getResources().openRawResource(R.raw.books);
-        String sxml = readTextFile(XmlFileInputStream);
-
-        Gson gson = new Gson();
-        GsonBooks response = gson.fromJson(sxml, GsonBooks.class);
 
         String s = "";
     }
@@ -93,4 +112,6 @@ public class MainActivity extends AppCompatActivity {
         return outputStream.toString();
     }
 }
+
+
 
