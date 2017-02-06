463357d9bcfbae1e7bcd9ba36c6b3ea8bf579e7b
d.doktorov <Pfec88lep>
2017-02-07 00:14:09 +0700

    FABRevealMenu

--- app/src/main/java/ekzeget/ru/ekzeget/HomeActivity.java
+++ app/src/main/java/ekzeget/ru/ekzeget/HomeActivity.java
@@ -1,5 +1,6 @@
 package ekzeget.ru.ekzeget;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -23,14 +24,14 @@ public class HomeActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -84,7 +85,8 @@ public class HomeActivity extends AppCompatActivity
         } else if (id == R.id.synopsis) {
 
         } else if (id == R.id.exegetes) {
-
+            Intent intent = new Intent(HomeActivity.this, ScrollingActivity.class);
+            startActivity(intent);
         } else if (id == R.id.links_generator) {
 
         } else if (id == R.id.guest_book) {
