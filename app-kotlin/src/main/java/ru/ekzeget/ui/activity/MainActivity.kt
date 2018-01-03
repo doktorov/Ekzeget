4cccd31a3018e609599b452a72d07a1ca9e66607
d.doktorov <Pfec88lep>
2018-01-04 00:03:14 +0700

    Main activity menu

--- app-kotlin/src/main/java/ru/ekzeget/ui/activity/MainActivity.kt
+++ app-kotlin/src/main/java/ru/ekzeget/ui/activity/MainActivity.kt
@@ -1,32 +1,28 @@
 package ru.ekzeget.ui.activity
 
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import ru.ekzeget.R
 
 class MainActivity : AppCompatActivity() {
-//    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-//        when (item.itemId) {
-//            R.id.navigation_home -> {
-//                message.setText(R.string.title_home)
-//                return@OnNavigationItemSelectedListener true
-//            }
-//            R.id.navigation_dashboard -> {
-//                message.setText(R.string.title_dashboard)
-//                return@OnNavigationItemSelectedListener true
-//            }
-//            R.id.navigation_notifications -> {
-//                message.setText(R.string.title_notifications)
-//                return@OnNavigationItemSelectedListener true
-//            }
-//        }
-//        false
-//    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_nz -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_vz -> {
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
     }
 }
